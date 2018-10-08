07cdce855ce44f1b660706f892066833cc6a78c9
Anzhelika <anzhelika110k@gmail.com>
2018-10-08 18:23:00 +0300

    refactor: rename function, replace var to let

--- src/index.js
+++ src/index.js
@@ -1,64 +1,55 @@
 class Sorter {
-  constructor() {
-    // your implementation
-	this.arr = [];
-  }
+    constructor() {
+        this.arr = [];
+    }
 
-  add(element) {
-    // your implementation
-	this.element = element;
-	this.arr.push(element);
-	return this.arr;
-  }
+    add(element) {
+        this.element = element;
+        this.arr.push(element);
+        return this.arr;
+    }
 
-  at(index) {
-    // your implementation
-	return this.arr[index];
-  }
+    at(index) {
+        return this.arr[index];
+    }
 
-  get length() {
-    // your implementation
-	return this.arr.length;
-  }
+    get length() {
+        return this.arr.length;
+    }
 
-  toArray() {
-    // your implementation
-	return Array.from(this.arr);
-  }
+    toArray() {
+        return Array.from(this.arr);
+    }
 
-  sort(indices) {
-    // your implementation	
-	this.indices = indices;
-	this.sort_arr=[];	
-	this.indices.sort(function(a, b) {
-		return a - b;
-	});	
-	for(var i in this.arr){
-		for(var j in this.indices){
-	
-			if(i==this.indices[j]){
-				this.sort_arr.push(this.arr[i]);
-			}
-			this.sort_arr.sort(this.SortFunction);	
-	
-			function S(a, b){
-				return a - b;
-			};
-			this.SortFunction = this.compareFunction || S;
-		
-			for(var k in this.sort_arr){
-				this.arr.splice(this.indices[k], 1, this.sort_arr[k]);	
-			}			
-		}  
-	}		
-	return this.arr;	
-  }
+    sort(indices) {
+        this.indices = indices;
+        this.sortArr = [];
+        this.indices.sort(function (a, b) {
+            return a - b;
+        });
+        for (let i in this.arr) {
+            for (let j in this.indices) {
+                if (i == this.indices[j]) {
+                    this.sortArr.push(this.arr[i]);
+                }
+                this.sortArr.sort(this.SortFunction);
 
-  setComparator(compareFunction) {
-    // your implementation
-	this.compareFunction = compareFunction;		
-	return this.arr;
-  }
+                function initialCompare(a, b) {
+                    return a - b;
+                };
+                this.SortFunction = this.compareFunction || initialCompare;
+                for (let k in this.sortArr) {
+                    this.arr.splice(this.indices[k], 1, this.sortArr[k]);
+                }
+            }
+        }
+        return this.arr;
+    }
+
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+        return this.arr;
+    }
 }
 
 module.exports = Sorter;
